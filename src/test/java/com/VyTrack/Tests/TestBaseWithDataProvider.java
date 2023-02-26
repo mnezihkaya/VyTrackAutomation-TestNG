6546cd4c09fceca1466d6f2a9840f624746fa7b0
nezih <m.nezihkaya@gmail.com>
2023-02-26 11:10:58 -0500

    DataProviders were added for DifferentUserTypes, allUsers, drivers, storeManagers, salesManagers, StoreAndSalesManagers

---
+++ src/test/java/com/VyTrack/Tests/TestBaseWithDataProvider.java
@@ -0,0 +1,71 @@
+package com.VyTrack.Tests;
+
+import com.VyTrack.Utilities.ConfigurationReader;
+import com.VyTrack.Utilities.Driver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
+public class TestBaseWithDataProvider {
+
+    @DataProvider(name = "DifferentUserTypes")
+    public Object[][] DifferentUserTypes() {
+        return new Object[][]{
+                {ConfigurationReader.getProperty("username1")},
+                {ConfigurationReader.getProperty("username2")},
+                {ConfigurationReader.getProperty("username3")},
+        };
+
+
+    }
+
+    @DataProvider(name = "AllUsers")
+    public Object[][] allUsers() {
+        return new Object[][]{
+                {ConfigurationReader.getProperty("username1")},
+                {ConfigurationReader.getProperty("username2")},
+                {ConfigurationReader.getProperty("username3")},
+                {ConfigurationReader.getProperty("username4")},
+                {ConfigurationReader.getProperty("username5")},
+                {ConfigurationReader.getProperty("username6")},
+        };
+    }
+    // Data for Drivers users
+    @DataProvider(name="drivers")
+    public Object[][] drivers(){
+        return new Object[][]{
+                {ConfigurationReader.getProperty("username1")},
+                {ConfigurationReader.getProperty("username4")},
+        };
+    }
+
+    // Data for Store Managers users
+    @DataProvider(name="storeManagers")
+    public Object[][] storeManagers(){
+        return new Object[][]{
+                {ConfigurationReader.getProperty("username2")},
+                {ConfigurationReader.getProperty("username5")},
+        };
+    }
+
+    // Data for Sales Managers users
+    @DataProvider(name="salesManagers")
+    public Object[][] salesManagers(){
+        return new Object[][]{
+                {ConfigurationReader.getProperty("username3")},
+                {ConfigurationReader.getProperty("username6")},
+        };
+    }
+
+    // Data for All Managers users
+    @DataProvider(name="managers")
+    public Object[][] StoreAndSalesManagers(){
+        return new Object[][]{
+                {ConfigurationReader.getProperty("username2")},
+                {ConfigurationReader.getProperty("username5")},
+                {ConfigurationReader.getProperty("username3")},
+                {ConfigurationReader.getProperty("username6")},
+        };
+    }
+
+}
