c23645cf2642a1e585bd8a344948e5a75be83ed8
nezih <m.nezihkaya@gmail.com>
2023-02-26 10:12:53 -0500

    Driver Utilities added

---
+++ src/test/java/com/VyTrack/Utilities/Driver.java
@@ -0,0 +1,63 @@
+package com.VyTrack.Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver() {
+    }    //created private constructor to stop access from outside of the class
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        try {
+            Thread.sleep(40);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (driver == null) {
+            String browserType = ConfigurationReader.getProperty("browser");
+            switch (browserType) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+
+            }
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
