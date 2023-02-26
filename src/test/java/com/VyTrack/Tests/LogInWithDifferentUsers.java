601a4a01d3da21fb7aca3e90f9c16d4619d29074
nezih <m.nezihkaya@gmail.com>
2023-02-26 13:01:18 -0500

    LogInWithDifferentUsers updated

--- src/test/java/com/VyTrack/Tests/LogInWithDifferentUsers.java
+++ src/test/java/com/VyTrack/Tests/LogInWithDifferentUsers.java
@@ -13,27 +13,45 @@ import java.util.List;
 public class LogInWithDifferentUsers extends TestBaseWithDataProvider {
 
     @Test(dataProvider = "managers")
-    //store and sales managers should view 8 module names.
+    //store and sales managers should view 8 module names.("Dashboards",
+    //                "Fleet", "Customers", "Sales", "Activities",
+    //                "Marketing", "Reports & Segments", "System")
     public void testMainModulesAppearedForManagers(String username) {
-
 
         LogInPage.login(username);
         MainPage mainPage = new MainPage();
 
-
-        List<String> ExpectedModuleNames = new ArrayList<>(Arrays.asList("Dashboards", "Fleet", "Customers", "Sales", "Activities", "Marketing", "Reports & Segments", "System"
-        ));
+        List<String> ExpectedModuleNames = new ArrayList<>(Arrays.asList("Dashboards",
+                "Fleet", "Customers", "Sales", "Activities",
+                "Marketing", "Reports & Segments", "System"));
 
         List<String> ActualModuleNames = new ArrayList<>();
         for (WebElement each : mainPage.allModules) {
             ActualModuleNames.add(each.getText());
         }
         //verify moduleNames
-        Assert.assertEquals(ActualModuleNames, ExpectedModuleNames);
-
+        Assert.assertEquals(ActualModuleNames,ExpectedModuleNames);
 
     }
 
+    //Drivers should view 4 module names.("Fleet", "Customers", "Activities", "System")
+    @Test(dataProvider = "drivers")
+    //store and sales managers should view 8 module names.
+    public void testMainModulesAppearedForDrivers(String username) {
+
+        LogInPage.login(username);
+        MainPage mainPage = new MainPage();
+
+        List<String> ExpectedModuleNames = new ArrayList<>(Arrays.asList("Fleet", "Customers", "Activities", "System"));
+
+        List<String> ActualModuleNames = new ArrayList<>();
+        for (WebElement each : mainPage.allModules) {
+            ActualModuleNames.add(each.getText());
+        }
+        //verify moduleNames
+        Assert.assertEquals(ActualModuleNames,ExpectedModuleNames);
+
+    }
 
 }
 
